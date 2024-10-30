eb15d94ec6ede99787c3fef84a9f1d3a1a7592c5
calltoaction <aledinatale82@gmail.com>
2024-10-30 15:10:25 +0100

    gsap-bundle

---
+++ gsap-bundle/src/index.js
@@ -0,0 +1,21 @@
+// Importa GSAP
+import { gsap } from "gsap";
+
+// Importa tutti i plugin di GSAP che ti servono
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+// Registra tutti i plugin con GSAP
+gsap.registerPlugin(Flip, ScrollTrigger, Observer, ScrollToPlugin);
+
+// Esportazione globale di GSAP e di tutti i plugin
+if (typeof window !== "undefined") {
+  window.gsap = gsap;
+  window.Flip = Flip;
+  window.ScrollTrigger = ScrollTrigger;
+  window.Observer = Observer;
+  window.ScrollToPlugin = ScrollToPlugin;
+}
+
---
+++ gsap-bundle/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',  
+  entry: './src/index.js',  // Punto di ingresso
+  output: {
+    filename: 'gsap-bundle.js',  // Nome del file di output
+    path: path.resolve(__dirname, 'dist'),  // Directory di output
+    library: 'GSAPBundle', // Nome del bundle esportato
+    libraryTarget: 'umd', // Target UMD per compatibilità con Webflow
+    globalObject: 'this' // Assicura compatibilità con ambienti browser
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,  // Applica questa regola ai file JavaScript
+        exclude: /node_modules/,  // Escludi i file nella cartella node_modules
+        use: {
+          loader: 'babel-loader',  // Usa babel-loader per trascrivere il JavaScript
+          options: {
+            presets: ['@babel/preset-env'],  // Preset per compatibilità con i browser
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,  // Abilita la minificazione per la produzione
+  },
+};
